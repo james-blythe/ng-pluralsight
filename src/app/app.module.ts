9d8feb47a67c42db599734bf418698ec196fec60
jBlythe <james.blythe.engineer@gmail.com>
2023-09-04 07:51:20 -0700

    Done until routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { NavBarComponent } from './nav/navbar.component';
+import { EventDetailsComponent } from './events/event-details/event-details.component'
+import { EventService } from './events/shared/event.service';
+import { ToastrService } from './common/toastr.service';
+import { appRoutes } from './routes'
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavBarComponent,
+    EventDetailsComponent,
+  ],
+  providers: [EventService, ToastrService],
+  bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
---
+++ src/app/events/event-thumbnail.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+
+@Component({
+	selector: 'event-thumbnail',
+	template: `
+    <div class="well hoverwell thumbnail">
+        <h2>{{event?.name}}</h2>
+        <div [ngStyle]="getStartTimeStyle()" class="well" ngSwitch="event?.time">
+					Date: {{event?.date}}
+					<span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+					<span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+					<span *ngSwitchDefault="">(Normal Start)</span>
+				</div>
+        <div>Price: \${{event?.price}}</div>
+        <div *ngIf="event?.location">
+            <span>Location: {{event?.location?.address}}</span>
+            <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
+        </div>
+    </div>
+`,
+	styles: [
+		`
+			.thumbnail { min-height: 210px; }
+			.pad-left { margin-left: 10px; }
+			.well div { color: #bbb; }
+			`
+	]
+})
+
+export class EventThumbnailComponent {
+	@Input() event: any
+	getStartTimeStyle(): any {
+		if (this.event && this.event.time === '8:00 am') {
+			return { color: '#003300', 'font-weight': 'bold' }
+		}
+		return {}
+	}
+}
